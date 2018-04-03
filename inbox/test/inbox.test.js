64bedfdd7254568d69ecdc226e95634f92bf80f7
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-04-03 06:27:25 -0500

    cleanup

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = Web3(ganache.provider());
+const web3 = new Web3(ganache.provider());
 
 class Car {
     park() {
