d609e5c56ff25c25702533c52f1de1ece8ae7946
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-04-03 21:28:46 -0500

    Added warm up test cases to test reading and writing to the test network

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -2,9 +2,11 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
+const INITIAL_STRING = 'Hi there!';
 let accounts;
 let inbox;
 
@@ -14,17 +16,34 @@ beforeEach(async () => {
 
     // use one of those accounts to deploy the contract
     inbox =  await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data:bytecode, arguments: ['Hi there!']})
-        .send({ from: accounts[0], gas: '1000000' })
+        .deploy({ data:bytecode, arguments: [INITIAL_STRING]})
+        .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
 
+    // Truthy test to make sure an address was assigned
     it('deploys a contract', () => {
-        console.log(inbox);
-        // assert.equal(car.park(), 'stopped');
+        assert.ok(inbox.options.address);
     });
 
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, INITIAL_STRING);
+    });
 
+    it('can change the message', async () => {
+        let newMessage = 'Hello';
+        let transactionHash = await inbox.methods.sendMessage(newMessage)
+            .send({
+                from: accounts[1],
+                gas: '1000000'
+            });
+
+        const message = await inbox.methods.message().call();
+        assert.equal(message, newMessage);
+    })
 
 });
