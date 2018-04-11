775a8510d9dbe345891fb2f71c892dabb3e99f84
Tapiwa <tapiwa.maruni@gmail.com>
2018-04-10 21:30:20 -0500

    Initial react JSX sample

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,1 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new component. Should produce some HTML
+const App = () => {
+    // return <div>Hi!</div>;
+
+    return <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+    </ol>
+}
+
+// Add components to DOM so user can view
+ReactDOM.render(<App />, document.querySelector('.container'));
