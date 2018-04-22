3edc0a23e5a31680d2ad8adb0ecd77b36c4dcaaa
Tapiwa <tapiwa.maruni@gmail.com>
2018-04-22 10:43:23 -0500

    Working with React classes

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />
-};
+class SearchBar extends Component {
+
+    render() {
+        return <div>
+                <label>Search </label>
+                {/*<input onChange={ this.onInputChange } />*/}
+                <input onChange={ event => console.log(event.target.value) } />
+            </div>
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+}
 
 
 export default SearchBar;
