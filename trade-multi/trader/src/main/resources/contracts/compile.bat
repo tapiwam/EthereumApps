e85612d72adbd6834eb235c5bf04ab40f8016344
Tapiwa Maruni <tapiwa.maruni@gmail.com>
2018-06-27 12:19:53 -0500

    Generated sample Java classes for uber contract

--- trade-multi/trader/src/main/resources/contracts/compile.bat
+++ trade-multi/trader/src/main/resources/contracts/compile.bat
@@ -2,6 +2,10 @@
 mkdir output
 rm output/*
 
-solc trade.sol --bin --abi --optimize -o ./output/
+rem solc trade.sol --bin --abi --optimize -o ./output/
+solc PositionContract.sol --bin --abi --optimize -o ./output/
 
-web3j solidity generate ./output/trade.bin ./output/trade.abi -o ../../java -p trader.contracts
+rem web3j solidity generate ./output/trade.bin ./output/trade.abi -o ../../java -p trader.contracts
+
+rem web3j solidity generate ./output/PositionContract.bin ./output/PositionContract.abi -o ../../java -p trader.contracts
+web3j solidity generate ./output/TradeContract.bin ./output/TradeContract.abi -o ../../java -p trader.contracts
